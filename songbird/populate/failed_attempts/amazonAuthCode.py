1507cfb5366e9071a7f8b7881e4c09248c38d38e
Maria Nuñez Reyes <73229261+marnunrey2@users.noreply.github.com>
2024-09-10 15:59:45 +0200

    Update amazonAuthCode.py

--- songbird/populate/failed_attempts/amazonAuthCode.py
+++ songbird/populate/failed_attempts/amazonAuthCode.py
@@ -2,9 +2,9 @@ import requests
 
 ### Paso 1. Authorization code
 
-client_id = "amzn1.application-oa2-client.7c0d83cb1c9d4f048bfa5e9b4f390806"
+client_id = "Amazon client id"
 client_secret = (
-    "amzn1.oa2-cs.v1.9287a294bb74d3afd078534e40484585d191601050eacaa08709598335ea5012"
+    "Amazon client secret"
 )
 auth_url = "https://www.amazon.com/ap/oa"
 
@@ -20,5 +20,4 @@ data = {
 response = requests.get(auth_url, data=data)
 
 print(auth_url + "?" + response.request.body)
-# https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.7c0d83cb1c9d4f048bfa5e9b4f390806&scope=profile&response_type=code&redirect_uri=https%3A%2F%2F127.0.0.1%3A8000%2F
 # Insert url in navigator, login with amazon credentials and copy the code
